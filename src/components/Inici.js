0a374d60d719d58a96fb93c7477514e0fa6caed1
LuisVargasCarrasco <146224193+LuisVargasCarrasco@users.noreply.github.com>
2024-11-18 17:10:17 +0100

    TecnoWorld

---
+++ src/components/Inici.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Inici.css"
+
+const Inici = () => {
+    return (
+        <div className="inici">
+            <div classname="categories">
+                <div classname="icones">
+                    
+                </div>
+
+            </div>
+
+        </div>
+    )
+};
+
+export default Inici;
